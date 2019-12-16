674b3c44e8c897e17b7ec65db6925d97e32e6f05
Nader <nader_abdelghani@hotmail.com>
2019-12-16 04:56:52 +0200

    Add favicon.ico Endpoint

--- api.py
+++ api.py
@@ -1,11 +1,17 @@
-import flask
+import os
 
-app = flask.Flask(__name__)
+from flask import Flask, send_from_directory
+
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Testing Prototype</h1>"
+
+@app.route('/favicon.ico')
+def fav():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 app.run()
