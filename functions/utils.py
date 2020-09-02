c725d63c8e602820c9978afcf7d2d60ce2b3dc7c
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2020-09-02 04:10:38 +0200

    Add Utility Functions Files

---
+++ functions/utils.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+def show_images(images, titles=None):
+    n_ims = len(images)
+    if titles is None: titles = ['(%d)' % i for i in range(1, n_ims + 1)]
+    fig = plt.figure()
+    n = 1
+    for image, title in zip(images, titles):
+        a = fig.add_subplot(1, n_ims, n)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+        n += 1
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+    plt.show()
